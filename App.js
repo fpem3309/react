f18173129e3f41d85513b0c2a4ceb90d72c70295
KMG <alsrhks2154@gmail.com>
2021-09-05 15:55:27 +0900

    weather

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from 'react-native';
+import Loading from './Loading';
+import * as Location from 'expo-location';
+import axios from 'axios';
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = '99b9591353235046b3a3a5b669cedfa1';
+
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  getWeather = async(latitude,longitude)=>{
+    const {
+      data : {
+        main : {temp},
+        weather
+      }
+    } = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`//metric = 섭씨 (temp = 섭씨온도로 바뀜)
+      );
+
+    //console.log(data);
+
+    this.setState({
+      isLoading:false,
+      condition:weather[0].main,
+      temp
+    })
+
+  };
+  getLocation = async() => {
+    try {
+      await Location.getForegroundPermissionsAsync(); 
+      const {
+        coords : {latitude , longitude}
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+      //this.setState({latitude : false});
+
+    } catch (error) {
+      Alert.alert("Can't find you", "so sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading, temp, condition} = this.state;
+  return isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition}/> ;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Weather.js
@@ -0,0 +1,136 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar} from 'react-native';
+import PropTypes from 'prop-types';
+import { LinearGradient } from 'expo-linear-gradient';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const weatherOpthions = {
+    Haze : {
+        iconName : "weather-hail",
+        gradient : ["#4DA0B0" , "#D39D38"],
+        title : "Haze",
+        subtitle : "헤이헤이 헤이즈"
+    },
+    Thunderstorm : {
+        iconName :"weather-lightning",
+        gradient : ["#373B44", "#4286F4"],
+        title : "Thunderstorm",
+        subtitle : "썬더 필 더 썬더"
+    },
+    Drizzle : {
+        iconName :"weather-hail",
+        gradient : ["#89F7FE", "#66A6FF"],
+        title : "Drizzle",
+        subtitle : "이건 뭔 날씨여"
+    },
+    Rain : {
+        iconName :"weather-rainy",
+        gradient : ["#00C6FB", "#005BEA"],
+        title : "Rain",
+        subtitle : "밖에 비온다 주륵주륵"
+    },
+    Snow : {
+        iconName :"weather-snowy",
+        gradient : ["#7DE2FC", "#B9B6E5"],
+        title : "Snow",
+        subtitle : "손이 꽁꽁꽁 발이 꽁꽁꽁"
+    },
+    Atmosphere : {
+        iconName :"weather-hail",
+        gradient : ["#89F7Fe", "#66A6FF"],
+        title : "Atmosphere",
+        subtitle : "이것도 뭔날씨여"
+    },
+    Clear : {
+        iconName :"weather-sunny",
+        gradient : ["#FF7300", "#FEF253"],
+        title : "Clear",
+        subtitle : "날씨 한번 지리네"
+    },
+    Clouds : {
+        iconName :"weather-cloudy",
+        gradient : ["#D7D2CC", "#304352"],
+        title : "Cloud",
+        subtitle : "날씨 한번 별로네"
+    },
+    Mist : {
+        iconName :"weather-hail",
+        gradient : ["#4DA0B0", "#D39D38"],
+        title : "Mist",
+        subtitle : "미스트가 뭔날씨여"
+    },
+    Dust : {
+        iconName : "weather-hail",
+        gradient : ["#4DA0B0", "#D39D38"],
+        title : "Dust",
+        subtitle : "더스트는 또 뭔날씨여"
+    }
+};
+
+export default function Weather({temp, condition}){
+    return(
+        <LinearGradient
+        // Background Linear Gradient
+        colors={weatherOpthions[condition].gradient}
+        style={styles.container}
+      >
+          <StatusBar barStyle="light-content"/>
+        <View style={styles.halfcontainer}>
+            <MaterialCommunityIcons size={95} name={weatherOpthions[condition].iconName} color="white"/>
+            <Text style={styles.temp}>{temp}º</Text>
+        </View>
+        <View style={{...styles.halfcontainer, ...styles.textContainer} }>
+            <Text style={styles.title}>{weatherOpthions[condition].title}</Text>
+            <Text style={styles.subtitle}>{weatherOpthions[condition].subtitle}</Text>
+        </View>
+        </LinearGradient>
+    );
+}
+
+Weather.propTypes = {
+    temp : PropTypes.number.isRequired,
+    condition : PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+    ]).isRequired
+};
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    temp :{
+        fontSize : 35,
+        color : 'white'
+    },
+    halfcontainer:{
+        flex:1,
+        justifyContent : 'center',
+        alignItems : 'center'
+    },
+    title : {
+        color : 'white',
+        fontSize : 44,
+        fontWeight : '300',
+        marginBottom : 10
+    },
+    subtitle : {
+        color : 'white',
+        fontSize : 24,
+        fontWeight : '600'
+    },
+    textContainer : {
+        paddingHorizontal : 20,
+        alignItems : "flex-start"
+    }
+})
